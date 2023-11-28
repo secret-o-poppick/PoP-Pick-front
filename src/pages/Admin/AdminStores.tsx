b8f525e3fa9f9c6167a95ba0ae0269b9fc6e2587
강한나 <hanna0175@gmail.com>
2023-11-29 00:25:26 +0900

    ♻️ refactor: Admin store search 컴포넌트로 분리

--- src/pages/Admin/AdminStores.tsx
+++ src/pages/Admin/AdminStores.tsx
@@ -1,67 +1,46 @@
-import AdminTitle from '@/components/AdminTitle';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { faker } from '@faker-js/faker';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom'
-import Input from '@/components/Input';
+import { useNavigate } from 'react-router-dom';
 import Button from '@/components/Button';
+import AdminTitle from '@/components/AdminTitle';
 import AdminStoreTable from '@/components/AdminStoreTable';
+import AdminStoreSearch from '@/components/AdminStoreSearch';
 import { Store } from '@/types/index';
 
 export default function AdminStores() {
   const navigate = useNavigate();
-  const [fakeData, setFakeData] = useState<Store[]>([
-    {
-      image: faker.image.url(),
-      author: faker.person.fullName(),
-      title: faker.lorem.words(),
-      startAt: faker.date.recent(),
-      endAt: faker.date.future().getTime(),
-      createdAt: faker.date.past().getTime(),
-      active: false,
-    },
-    {
-      image: faker.image.url(),
-      author: faker.person.fullName(),
-      title: faker.lorem.words(),
-      startAt: faker.date.recent(),
-      endAt: faker.date.future().getTime(),
-      createdAt: faker.date.past().getTime(),
-      active: false,
-    },
-  ]);
-  const [searchText, setSearchText] = useState('');
+  const [fakeData, setFakeData] = useState<Store[]>(generateFakeData(2));
   const [filteredData, setFilteredData] = useState<Store[]>(fakeData);
 
-  const handleEdit = () => {
-    navigate('#')
-  }
-
-  const handleDelete = () => {
-    navigate('#')
-  }
-
+  const handleEdit = () => navigate('#');
+  const handleDelete = () => navigate('#');
   const activeBtn = (index: number) => {
     const updatedData = [...fakeData];
     updatedData[index].active = !updatedData[index].active;
     setFakeData(updatedData);
-  }
+  };
 
-
-  const handleSearch = () => {
+  const handleSearch = (text: string) => {
     const filtered = fakeData.filter(
       (store) =>
-        store.author.toLowerCase().includes(searchText.toLowerCase()) ||
-        store.title.toLowerCase().includes(searchText.toLowerCase())
+        store.author.toLowerCase().includes(text.toLowerCase()) ||
+        store.title.toLowerCase().includes(text.toLowerCase())
     );
     setFilteredData(filtered);
   };
 
-  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-      handleSearch();
-    }
-  };
+  function generateFakeData(count: number): Store[] {
+    return Array.from({ length: count }, () => ({
+      image: faker.image.url(),
+      author: faker.person.fullName(),
+      title: faker.lorem.words(),
+      startAt: faker.date.recent(),
+      endAt: faker.date.future().getTime(),
+      createdAt: faker.date.past().getTime(),
+      active: false,
+    }));
+  }
 
   return (
     <>
@@ -70,7 +49,7 @@ export default function AdminStores() {
 
       {/* 등록버튼 */}
       <AddBtn>
-        <Button color='primary' onClick={handleSearch}>
+        <Button color='primary' onClick={() => handleSearch('')}>
           등록
         </Button>
       </AddBtn>
@@ -84,19 +63,8 @@ export default function AdminStores() {
       />
 
       {/* 검색창 */}
-      <StyledSearch>
-        <div>
-          <Input
-            type='text'
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-            onKeyDown={handleKeyPress}
-          />
-        </div>
-        <Button color='primary' onClick={handleSearch}>
-          검색
-        </Button>
-      </StyledSearch>
+      <AdminStoreSearch handleSearch={handleSearch} />
+
       {/* 페이지네이션 */}
     </>
   );
@@ -107,27 +75,8 @@ const AddBtn = styled.div`
   display: flex;
   justify-content: flex-end;
   margin-bottom: 1.2em;
-  
+
   Button {
     width: 80px;
   }
-`
-
-const StyledSearch = styled.div`
- display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  text-align: center;
-  margin-top: 3em;
-
-& Input {
-  width:220px;
-}
-
-Button {
-  width:80px;
-  margin-left: 0.6em;
-}
-`
-
+`;
