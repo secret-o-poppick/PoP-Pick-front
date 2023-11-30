760579fa0c9ea143c6ace32511dc01803758cdc8
강한나 <hanna0175@gmail.com>
2023-11-30 18:34:42 +0900

    🌱 add: Admin 등록페이지5

--- src/components/StoreCreateStep5.tsx
+++ src/components/StoreCreateStep5.tsx
@@ -1,21 +1,77 @@
-import React from "react";
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import Input from '@/components/Input';
 
 interface StoreCreateStepProps {
     handleChange: (input: string) => (e: React.ChangeEvent<HTMLInputElement>) => void;
     nextStep: () => void;
 }
 
-const StoreCreateStep2: React.FC<StoreCreateStepProps> = (props) => {
+const StoreCreateStep5: React.FC<StoreCreateStepProps> = () => {
+    const [brand, setBrand] = useState('');
+
     return (
-        <Step>
-            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-        </Step>
+        <>
+            <StyledContent>
+                <StyledInput>
+                    <InputStyled value={brand} type='text' onChange={(e) => setBrand(e.target.value)} label='SNS(공식페이지)' />
+                </StyledInput>
+                <StyledInput>
+                    <InputStyled value={brand} type='text' onChange={(e) => setBrand(e.target.value)} label='이벤트/프로모션' />
+                </StyledInput>
+                <StyledInput>
+                    <StyledInputRow>
+                        <label>설명글</label>
+                        <StyledTextArea />
+                    </StyledInputRow>
+                </StyledInput>
+                <StyledInput>
+                    <InputStyled value={brand} type='text' onChange={(e) => setBrand(e.target.value)} label='기타' />
+                </StyledInput>
+            </StyledContent>
+        </>
     );
 };
 
-const Step = styled.div`
-display:flex
-`
+const StyledContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 500px;
+    row-gap: 3em;
+`;
 
-export default StoreCreateStep2;
+const StyledInput = styled.div`
+    /* height: 5em; */
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 0.4rem;
+`;
+
+const StyledInputRow = styled(StyledInput)`
+    flex-direction: row;
+    width:100%;
+
+    & label {
+        color:#334155;
+        width: 218px;
+        font-weight:700;
+    }
+`;
+
+const InputStyled = styled(Input)`
+    width: 100%;
+`;
+
+const StyledTextArea = styled.textarea`
+    resize: none;
+    width: 100%;
+    height: 200px;
+    box-sizing: border-box;
+    border: 1px solid #888;
+    border-radius: 5px;
+    padding: 0.8rem 1.2rem;
+    font-size: 1rem;
+`;
+
+export default StoreCreateStep5;
