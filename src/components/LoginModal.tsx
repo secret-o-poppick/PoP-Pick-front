920c90f743824d262f33b1459044c5895ce010d5
kcjfgnl9205 <kcjfgnl9205@gmail.com>
2023-12-01 21:30:31 +0900

    🐛 fix: 로그인 모달 제목 추가

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -4,16 +4,23 @@ import KakaoLoginButton from '@/components/SocialLoginButton/KakaoLoginButton';
 export default function LoginModal() {
   return (
     <Container>
+      <H1>소셜 로그인</H1>
       <KakaoLoginButton />
     </Container>
   );
 }
 
+const H1 = styled.h1`
+  font-size: 1.8rem;
+  font-weight: 700;
+  color: #000;
+`;
+
 const Container = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 2rem;
-  padding: 6rem 4rem 5rem 4rem;
+  padding: 4rem 2rem 3rem 2rem;
 `;
