718eb7c8781cc8b58238d6b3e350356f36ee26d1
kcjfgnl9205 <kcjfgnl9205@gmail.com>
2023-12-04 20:20:43 +0900

    🐛 fix: 카카오 로그인 수정

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '@/context/AuthContext';
 
 export default function PrivateRoute() {
   const { isLoggedIn } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 0);
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   if (isLoggedIn) {
-    // 인증이 반드시 필요한 페이지
     return <Outlet />;
   }
 
