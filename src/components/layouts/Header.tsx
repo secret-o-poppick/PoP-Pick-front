d7c7e5de89095e026ab97594d0683b652373e153
강한나 <hanna0175@gmail.com>
2023-12-04 17:51:25 +0900

    🌱 add: 날짜형 변환

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -66,7 +66,7 @@ export default function Header() {
         const year = date.getFullYear();
         const month = String(date.getMonth() + 1).padStart(2, '0');
         const day = String(date.getDate()).padStart(2, '0');
-        return `${year}${month}${day}`;
+        return `${year}-${month}-${day}`;
       };
 
       const fromDateString = newRange.from ? formatDateString(newRange.from) : '';
